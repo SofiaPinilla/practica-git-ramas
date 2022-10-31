897620103801b8f4d74b6dbc1ea3d76fa987125b
SofiaPinilla <contact@sofiapinilla.com>
2022-10-31 09:14:49 +0100

    endpoint get productos and post product created

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require("express");
 const app = express()
 const PORT = 3001
 
+app.get("/",(req,res)=>{
+    res.send("Todos los productos")
+})
+app.post("/",(req,res)=>{
+    res.send("creando un producto")
+})
+
 app.listen(PORT,()=> console.log('Servidor levantado en puerto '+PORT))
 // app.listen(PORT,()=> console.log(`Servidor levantado en puerto ${PORT}`))
