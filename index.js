f93f40308d063bc0e556fecc53b285f2ac99a81c
SofiaPinilla <contact@sofiapinilla.com>
2022-10-31 09:17:12 +0100

    endpoint get categories created

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const express = require("express");
 const app = express()
 const PORT = 3001
 
+app.get("/",(req,res)=>{
+    res.send("todas las categorias")
+})
+
 app.listen(PORT,()=> console.log('Servidor levantado en puerto '+PORT))
 // app.listen(PORT,()=> console.log(`Servidor levantado en puerto ${PORT}`))
